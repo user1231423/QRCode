99e61c895b9010bd12a28f987eeeb7d35346c767
Eduardo Gouveia <jevg@live.com.pt>
2020-07-13 18:42:45 +0100

    Start

---
+++ TestingQR/Controllers/ValuesController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using QRCoder;
+using TestingQR.Models;
+
+namespace TestingQR.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpPost]
+        public ActionResult GenerateQRCode(QRCodeGenerate qRCodeGenerate)
+        {
+            QRCodeGenerator _qrCode = new QRCodeGenerator();
+            QRCodeData _qrCodeData = _qrCode.CreateQrCode(qRCodeGenerate.QRTxt, QRCodeGenerator.ECCLevel.Q);
+            QRCode qrCode = new QRCode(_qrCodeData);
+            Bitmap qrCodeImage = qrCode.GetGraphic(20);
+            return Ok(BitmapToBytesCode(qrCodeImage));
+        }
+
+        private protected Byte[] BitmapToBytesCode(Bitmap image)
+        {
+            using (MemoryStream stream = new MemoryStream())
+            {
+                image.Save(stream, System.Drawing.Imaging.ImageFormat.Png);
+                return stream.ToArray();
+            }
+        }
+    }
+}
